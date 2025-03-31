bbcd51e6722fe97b543f6083997ca4242386953e
Vincent Bollaert <vincentbollaert@gmail.com>
2025-03-31 20:27:36 +0200

    hook up login and auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import postgres from 'postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { AuthError } from 'next-auth'
+import { signIn } from '@/auth'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -106,4 +108,23 @@ export async function deleteInvoice(id: string) {
     console.log(`deleteInvoice error, '${error}`)
   }
   revalidatePath('/dashboard/invoices')
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
